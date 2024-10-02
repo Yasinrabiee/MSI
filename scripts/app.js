2f7cda21d10ec12987da028127d8684c78ac1444
Yasin Rabiee <hlww6303@gmail.com>
2024-10-02 10:18:04 -0700

    init project

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+let btn = $(`.header__icon i`);
+
+$(`.header__icon`).click(function() {
+	if(btn.hasClass(`fa fa-bars`))
+	{
+		$(`.nav__menu`).css(`left`,`0`);
+		btn.removeClass(`fa fa-bars`).addClass(`fa fa-times`);
+	}
+	
+	else
+	{
+		$(`.nav__menu`).css(`left`,`-255px`);
+		btn.removeClass(`fa fa-times`).addClass(`fa fa-bars`);
+	}
+})
