167895f63db7141220c075ab12020433ad12747e
Yasin Rabiee <hlww6303@gmail.com>
2024-10-03 11:43:59 -0700

    add new feature: Lazy Loading

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,32 @@
-const btn = $(`.header__icon i`);
+const menu = $(`.header__icon`);
 
-$(`.header__icon`).click(function() {
-	if(btn.hasClass(`fa fa-bars`)) {
-		$(`.nav__menu`).css(`left`,`0`);
-		btn.removeClass(`fa fa-bars`).addClass(`fa fa-times`);
+$(`.header__icon svg`).click(function() {
+	menu.toggleClass(`active`)
+	if(menu.hasClass(`active`)) {
+		$(`.nav__menu`).css(`right`,`0`);
+		$(`.header__icon svg:first-child`).css(`display`, `none`);
+		$(`.header__icon svg:last-child`).css(`display`, `block`);
+
 	} else {
-		$(`.nav__menu`).css(`left`,`-255px`);
-		btn.removeClass(`fa fa-times`).addClass(`fa fa-bars`);
+		$(`.nav__menu`).css(`right`,`-300px`);
+		$(`.header__icon svg:first-child`).css(`display`, `block`);
+		$(`.header__icon svg:last-child`).css(`display`, `none`);
 	}
+});
+
+const lazyLoadingElem = $(`.lazy-loading`);
+console.log(lazyLoadingElem);
+
+function loadElements() {
+	lazyLoadingElem.each(function() {
+		const elem = $(this);
+		if(elem.offset().top < $(window).scrollTop() + $(window).height())
+			elem.addClass(`loaded`);
+	});
+}
+
+loadElements();
+
+$(window).on(`scroll`, function() {
+	loadElements();
 });
