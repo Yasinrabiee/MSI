0227caba90fabd3004e7eaeea2c819cc3db4aee1
Yasin Rabiee <hlww6303@gmail.com>
2024-10-03 07:25:56 -0700

    fix bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,10 @@
 const btn = $(`.header__icon i`);
 
 $(`.header__icon`).click(function() {
-	if(btn.hasClass(`fa fa-bars`))
-	{
+	if(btn.hasClass(`fa fa-bars`)) {
 		$(`.nav__menu`).css(`left`,`0`);
 		btn.removeClass(`fa fa-bars`).addClass(`fa fa-times`);
-	}
-	
-	else
-	{
+	} else {
 		$(`.nav__menu`).css(`left`,`-255px`);
 		btn.removeClass(`fa fa-times`).addClass(`fa fa-bars`);
 	}
